f63ad018a5a0c3ec33e9fbe4a61dd05649a5e799
Peter Baldwin <pbaldwin@clearfoundation.com>
2011-03-15 04:33:06 +0000

    - Removed old menu configuration

--- deploy/menu.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$menu['/app/intrusion_detection']['title'] = menu('intrusion_detection', 'intrusion_detection');
-$menu['/app/intrusion_detection']['category'] = menu('menu', 'menu_category_gateway');
-$menu['/app/intrusion_detection']['subcategory'] = menu('menu', 'menu_subcategory_intrusion_protection');
